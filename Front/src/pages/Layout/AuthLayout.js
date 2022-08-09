9e47ae110c3748659863622216f4bbd08c9bec46
Robuste Manohisafidy <robustemmanuel@gmail.com>
2022-08-09 19:55:46 +0300

    Auth Layout

---
+++ Front/src/pages/Layout/AuthLayout.js
@@ -0,0 +1,14 @@
+import { Box, Grid, makeStyles } from '@mui/material'
+import React from 'react'
+
+const AuthLayout = () => {
+  return (
+    <Grid container width="100vw" height="100vh" alignItems="center" justifyContent="center">
+        <Box width="50px" height="50px" sx={{backgroundColor:"red"}}>
+            
+        </Box>
+    </Grid>
+  )
+}
+
+export default AuthLayout
--- Front/src/router/index.js
+++ Front/src/router/index.js
@@ -2,12 +2,14 @@ import React from "react"
 import { BrowserRouter,Route,Routes } from "react-router-dom"
 import UserLayout from "../pages/Layout"
 import AdminLayout from "../Back_office/pages/layout"
+import AuthLayout from "../pages/Layout/AuthLayout"
 
 const AppRouter=()=>{
      return(
           <BrowserRouter>
                     <Routes>
                          <Route path='admin/*' element={<AdminLayout/>} />
+                         <Route path='auth/*' element={<AuthLayout/>}/>
                          <Route path='/*' element={<UserLayout/>} />
                     </Routes>
               
